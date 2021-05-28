4e17d19d529b41cb94faffd03f2ceff498623e1f
Muetzilla <muetz-github@gasbichler.ch>
2021-05-28 23:10:40 +0200

    get inputs

--- js/average.js
+++ js/average.js
@@ -1,13 +1,25 @@
 function calculateAverage() {
     var averageInput = document.getElementById("average");
-    var idnumber = 1;
-    var inputVal = document.getElementById("inputNote" + idnumber).value;
-    var inputValField = document.getElementById("inputNote1");
-
-    if (inputVal == "") {
-        averageInput.value = "NaN";
-        console.log("NaN");
-    } else {
+    var idnumber;
+    var inputVal;
+    var inputValField;
+    var inputGewichtung;
+    var inputGewichtungFiled;
+    var stringElementNoteId;
+    var stringElementGewichtungId;
+    var inputs = document.getElementsByClassName("notenInput");
+    for(var i = 0, length = inputs.length; i < length; i++){
+        idnumber = i + 1;
+        stringElementNoteId = "inputNote" + idnumber;
+        stringElementGewichtungId = "inputWert" + idnumber;
+        inputVal = document.getElementById(stringElementNoteId).value;
+        inputValField = document.getElementById(stringElementNoteId);
+        inputGewichtung = document.getElementById(stringElementGewichtungId).value;
+        inputGewichtung = document.getElementById(stringElementGewichtungId);
+        console.log(inputGewichtungFiled);
+        console.log(inputGewichtung);
+        console.log(inputVal);
+        console.log(inputValField);
         if (inputVal > 6 || inputVal < 1) {
             inputValField.classList.remove("correctNoteInput"); 
             inputValField.className += " wrongNoteInput";
@@ -15,9 +27,14 @@ function calculateAverage() {
             averageInput.value = inputVal;
             inputValField.classList.remove("wrongNoteInput"); 
             inputValField.className += " correctNoteInput";
-
+            if (inputVal != "") {
+               
+            } else {
+                
+            }
         }
     }
+   
 
 }
 function addColum() {
