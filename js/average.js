567634618df1a1028ef1a565683e5647cbf1026a
Muetzilla <muetz-github@gasbichler.ch>
2021-05-28 23:35:48 +0200

    ups value undefined

--- js/average.js
+++ js/average.js
@@ -7,34 +7,41 @@ function calculateAverage() {
     var inputGewichtungFiled;
     var stringElementNoteId;
     var stringElementGewichtungId;
+    var calculationAverageNote;
+    var calculateAverageGewichtung;
     var inputs = document.getElementsByClassName("notenInput");
-    for(var i = 0, length = inputs.length; i < length; i++){
+    for (var i = 0, length = inputs.length; i < length; i++) {
         idnumber = i + 1;
         stringElementNoteId = "inputNote" + idnumber;
         stringElementGewichtungId = "inputWert" + idnumber;
-        inputVal = document.getElementById(stringElementNoteId).value;
         inputValField = document.getElementById(stringElementNoteId);
-        inputGewichtung = document.getElementById(stringElementGewichtungId).value;
         inputGewichtung = document.getElementById(stringElementGewichtungId);
         console.log(inputGewichtungFiled);
         console.log(inputGewichtung);
         console.log(inputVal);
         console.log(inputValField);
         if (inputVal > 6 || inputVal < 1) {
-            inputValField.classList.remove("correctNoteInput"); 
+            inputValField.classList.remove("correctNoteInput");
             inputValField.className += " wrongNoteInput";
         } else {
-            averageInput.value = inputVal;
-            inputValField.classList.remove("wrongNoteInput"); 
+            inputValField.classList.remove("wrongNoteInput");
             inputValField.className += " correctNoteInput";
             if (inputVal != "") {
-               
+                inputGewichtung = document.getElementById(stringElementGewichtungId).value;
+                inputVal = document.getElementById(stringElementNoteId).value;
+                calculationAverageNote += inputVal * (inputGewichtung / 100);
+                calculateAverageGewichtung += inputGewichtung;
+                console.log("Gewichtung: " + calculateAverageGewichtung);
+                console.log("Note: " + calculationAverageNote);
             } else {
-                
+
             }
         }
     }
-   
+    console.log(calculationAverageNote / calculateAverageGewichtung);
+    averageInput.value = calculationAverageNote / calculateAverageGewichtung;
+
+
 
 }
 function addColum() {
