6a2e0599e380259b83426f0b3dae85b133554286
Muetzilla <muetz-gitkraken@gasbichler.ch>
2021-05-28 08:50:26 +0200

    add new row

--- js/average.js
+++ js/average.js
@@ -2,5 +2,6 @@ function calculateAverage(){
     var averageInput = document.getElementById("average").value = 1234
 }
 function addColum(){
-
+   var notenDiv = document.getElementById("noten");
+   notenDiv.append("New row");
 }
