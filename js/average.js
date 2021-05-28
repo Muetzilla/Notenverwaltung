15075b5cec834b241e34f1f022f6a21d99967365
Muetzilla <muetz-github@gasbichler.ch>
2021-05-28 13:10:40 +0200

    Yee its working

--- js/average.js
+++ js/average.js
@@ -1,7 +1,26 @@
-function calculateAverage(){
-    var averageInput = document.getElementById("average").value = 1234
+function calculateAverage() {
+    var averageInput = document.getElementById("average");
+    var idnumber = 1;
+    var inputVal = document.getElementById("inputNote" + idnumber).value;
+    var inputValField = document.getElementById("inputNote1");
+
+    if (inputVal == "") {
+        averageInput.value = "NaN";
+        console.log("NaN");
+    } else {
+        if (inputVal > 6 || inputVal < 1) {
+            inputValField.classList.remove("correctNoteInput"); 
+            inputValField.className += " wrongNoteInput";
+        } else {
+            averageInput.value = inputVal;
+            inputValField.classList.remove("wrongNoteInput"); 
+            inputValField.className += " correctNoteInput";
+
+        }
+    }
+
 }
-function addColum(){
-   var notenDiv = document.getElementById("noten");
-   notenDiv.append("New row ");
+function addColum() {
+    var notenDiv = document.getElementById("noten");
+    notenDiv.append("New row");
 }
